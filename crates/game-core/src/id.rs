e8440abf2134a3e364db9bdadf9ce3f2c43e263c
Gautham Ganapathy <gauthamg@gmail.com>
2026-08-05 07:01:30 +0100

    Ids

--- crates/game-core/src/id.rs
+++ crates/game-core/src/id.rs
@@ -1,1 +1,30 @@
-//! Strongly typed domain identifiers.
+macro_rules! uuid_id {
+    ($name:ident) => {
+        #[derive(
+            Debug,
+            Clone,
+            Copy,
+            PartialEq,
+            Eq,
+            PartialOrd,
+            Ord,
+            Hash,
+            serde::Serialize,
+            serde::Deserialize,
+        )]
+        pub struct $name(pub uuid::Uuid);
+
+        impl $name {
+            #[must_use]
+            pub fn new() -> Self {
+                Self(uuid::Uuid::new_v4())
+            }
+        }
+    };
+}
+
+uuid_id!(GameId);
+uuid_id!(RoundId);
+uuid_id!(PlayerId);
+uuid_id!(MeldId);
+uuid_id!(CardId);
