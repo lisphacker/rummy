a80c868dfcd35c0faa65b4e241692c033994577e
Gautham Ganapathy <gauthamg@gmail.com>
2026-08-06 08:40:40 +0100

    constructors

--- crates/game-core/src/meld.rs
+++ crates/game-core/src/meld.rs
@@ -15,23 +15,52 @@ pub enum MeldType {
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct Meld {
     meld_type: MeldType,
-    cards: Vec<CardId>,
+    card_ids: Vec<CardId>,
 }
 
 impl Meld {
     pub fn new_set(cards: &[Card]) -> GameResult<Self> {
-        todo!()
+        let (rank, suits) = validate_set_cards(cards)?;
+        let card_ids = validate_unique_card_ids(cards)?;
+        Ok(Self {
+            meld_type: MeldType::Set { rank, suits },
+            card_ids,
+        })
     }
 
     pub fn new_run(cards: &[Card]) -> GameResult<Self> {
+        let (suit, start, end) = validate_run_cards(cards)?;
+        let card_ids = validate_unique_card_ids(cards)?;
+        Ok(Self {
+            meld_type: MeldType::Run { suit, start, end },
+            card_ids,
+        })
+    }
+
+    pub fn add(&mut self, card: Card) -> GameResult<()> {
+        match &mut self.meld_type {
+            MeldType::Set { .. } => self.add_to_set(card),
+            MeldType::Run { .. } => self.add_to_run(card),
+        }
+    }
+
+    fn add_to_set(&mut self, card: Card) -> GameResult<()> {
         todo!()
     }
 
-    pub fn add_to_set(card: Card) -> GameResult<()> {
+    fn add_to_run(&mut self, card: Card) -> GameResult<()> {
         todo!()
     }
+}
 
-    pub fn add_to_run(card: Card) -> GameResult<()> {
-        todo!()
-    }
+fn validate_set_cards(cards: &[Card]) -> GameResult<(Rank, HashSet<Suit>)> {
+    todo!()
+}
+
+fn validate_run_cards(cards: &[Card]) -> GameResult<(Suit, Rank, Rank)> {
+    todo!()
+}
+
+fn validate_unique_card_ids(cards: &[Card]) -> GameResult<Vec<CardId>> {
+    todo!()
 }
