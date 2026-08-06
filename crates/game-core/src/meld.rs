9ab9e920edf72dc2f879102ab1336b2791ecefec
Gautham Ganapathy <gauthamg@gmail.com>
2026-08-06 03:44:26 +0100

    Meld types

--- crates/game-core/src/meld.rs
+++ crates/game-core/src/meld.rs
@@ -1,1 +1,13 @@
-//! Meld domain types.
+use crate::id::CardId;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+pub enum MeldType {
+    Set,
+    Run,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+pub struct Meld {
+    pub meld_type: MeldType,
+    pub cards: Vec<CardId>,
+}
