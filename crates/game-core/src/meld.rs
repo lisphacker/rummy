1dcafe6fa51870c406da549a97d26371908ba90c
Gautham Ganapathy <gauthamg@gmail.com>
2026-08-15 01:50:48 +0100

    fixed add_rejects_a_fifth_card_when_a_set_contains_jokers

--- crates/game-core/src/meld.rs
+++ crates/game-core/src/meld.rs
@@ -16,24 +16,27 @@ pub enum MeldType {
 pub struct Meld {
     meld_type: MeldType,
     card_ids: Vec<CardId>,
+    num_joker_cards: usize,
 }
 
 impl Meld {
     pub fn new_set(cards: &[Card]) -> GameResult<Self> {
-        let (rank, suits) = validate_set_cards(cards, true)?;
+        let (rank, suits, num_joker_cards) = validate_set_cards(cards, true)?;
         let card_ids = validate_unique_card_ids(cards)?;
         Ok(Self {
             meld_type: MeldType::Set { rank, suits },
             card_ids,
+            num_joker_cards,
         })
     }
 
     pub fn new_run(cards: &[Card]) -> GameResult<Self> {
-        let (suit, start, end) = validate_run_cards(cards, true)?;
+        let (suit, start, end, num_joker_cards) = validate_run_cards(cards, true)?;
         let card_ids = validate_unique_card_ids(cards)?;
         Ok(Self {
             meld_type: MeldType::Run { suit, start, end },
             card_ids,
+            num_joker_cards,
         })
     }
 
@@ -41,16 +44,6 @@ impl Meld {
         if self.card_ids.contains(&card.id) {
             return error(MeldError::CardAlreadyInMeld);
         }
-        let num_joker_cards = self
-            .card_ids
-            .iter()
-            .filter(|&&card_id| {
-                matches!(
-                    card_id,
-                    _ if matches!(card.face, crate::card::CardFace::Joker)
-                )
-            })
-            .count();
         match &mut self.meld_type {
             MeldType::Set { rank, suits } => {
                 match card.face {
@@ -64,13 +57,13 @@ impl Meld {
                         if suits.contains(&suit) {
                             return error(MeldError::SetMustHaveUniqueSuits);
                         }
-                        if suits.len() + num_joker_cards + 1 > 4 {
+                        if suits.len() + self.num_joker_cards + 1 > 4 {
                             return error(MeldError::SetCannotHaveMoreThanFourCards);
                         }
                         suits.insert(suit);
                     }
                     crate::card::CardFace::Joker => {
-                        if suits.len() + num_joker_cards + 1 > 4 {
+                        if suits.len() + self.num_joker_cards + 1 > 4 {
                             return error(MeldError::SetCannotHaveMoreThanFourCards);
                         }
                     }
@@ -132,7 +125,10 @@ fn error<T>(e: MeldError) -> GameResult<T> {
     Err(GameError::MeldError(e))
 }
 
-fn validate_set_cards(cards: &[Card], require_complete: bool) -> GameResult<(Rank, HashSet<Suit>)> {
+fn validate_set_cards(
+    cards: &[Card],
+    require_complete: bool,
+) -> GameResult<(Rank, HashSet<Suit>, usize)> {
     if require_complete && cards.len() < 3 {
         return error(MeldError::NotEnoughCardsForMeld);
     }
@@ -179,10 +175,13 @@ fn validate_set_cards(cards: &[Card], require_complete: bool) -> GameResult<(Ran
         return error(MeldError::SetHasTooManyJokers);
     }
 
-    Ok((ranks[0], suits))
+    Ok((ranks[0], suits, num_joker_cards))
 }
 
-fn validate_run_cards(cards: &[Card], require_complete: bool) -> GameResult<(Suit, Rank, Rank)> {
+fn validate_run_cards(
+    cards: &[Card],
+    require_complete: bool,
+) -> GameResult<(Suit, Rank, Rank, usize)> {
     if require_complete && cards.len() < 3 {
         return error(MeldError::NotEnoughCardsForMeld);
     }
@@ -246,6 +245,7 @@ fn validate_run_cards(cards: &[Card], require_complete: bool) -> GameResult<(Sui
     if !sorted_non_ace_ranks.is_empty() {
         let mut start = sorted_non_ace_ranks[0];
         let mut end = sorted_non_ace_ranks[0];
+        let mut num_joker_cards = num_joker_cards;
 
         let mut remaining_jokers = num_joker_cards;
         for &rank in sorted_non_ace_ranks.iter().skip(1) {
@@ -254,6 +254,7 @@ fn validate_run_cards(cards: &[Card], require_complete: bool) -> GameResult<(Sui
                 end = rank;
             } else if d - 1 <= remaining_jokers {
                 remaining_jokers -= d - 1;
+                num_joker_cards += d - 1;
                 end = rank;
             } else {
                 return error(MeldError::RunMustHaveConsecutiveRanks);
@@ -266,6 +267,7 @@ fn validate_run_cards(cards: &[Card], require_complete: bool) -> GameResult<(Sui
             if d - 1 <= remaining_jokers {
                 num_unused_ace_ranks -= 1;
                 remaining_jokers -= d - 1;
+                num_joker_cards += d - 1;
                 start = Rank::Ace;
             }
         }
@@ -274,6 +276,7 @@ fn validate_run_cards(cards: &[Card], require_complete: bool) -> GameResult<(Sui
             if d - 1 <= remaining_jokers {
                 num_unused_ace_ranks -= 1;
                 remaining_jokers -= d - 1;
+                num_joker_cards += d - 1;
                 end = Rank::Ace;
             }
         }
@@ -297,11 +300,11 @@ fn validate_run_cards(cards: &[Card], require_complete: bool) -> GameResult<(Sui
         if num_unused_ace_ranks > 0 {
             return error(MeldError::RunMustHaveConsecutiveRanks);
         }
-        Ok((suits[0], start, end))
+        Ok((suits[0], start, end, num_joker_cards))
     } else if num_ace_ranks == 1 && num_joker_cards >= 2 {
         let start = Rank::Ace;
         match incr_rank(start, num_joker_cards) {
-            Some(end) => Ok((suits[0], start, end)),
+            Some(end) => Ok((suits[0], start, end, num_joker_cards)),
             None => error(MeldError::RunMustHaveConsecutiveRanks),
         }
     } else {
